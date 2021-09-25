0951fc793a11b46123e224224381f811d66430b9
Ladislav Szolik <szolik.ladislav@gmail.com>
2021-09-25 16:37:47 +0200

    fix home link

--- pages/success.js
+++ pages/success.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '../styles/Success.module.css';
 
 function Success() {
   return (
     <div className={styles.container}>
       <h1>Thanks for joining! You will hear from us.</h1>
-      <a href="/">Back to home</a>
+
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
     </div>
   );
 }

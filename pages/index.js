fd7750ab8dfa41aa96a926d7e655569b7bfd5f46
Ladislav Szolik <szolik.ladislav@gmail.com>
2021-09-25 16:24:19 +0200

    add sign up form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -8,15 +7,35 @@ export default function Home() {
       <Head>
         <title>CodersWhoDesign</title>
         <meta name="description" content="A communitiy of status quo challengers" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome!</h1>
+        <h1 className={styles.title}>Coders who design</h1>
 
         <p className={styles.description}>
-          We are preparing a movement to connect coders who want to design.
+          We are preparing a movement connecting all coders who want more.
         </p>
+
+        <form
+          className={styles.signUpFrom}
+          name="earlyaccess"
+          method="POST"
+          data-netlify="true"
+        >
+          <h2 className={styles.subTitle}>Get early updates</h2>
+          <p className={styles.description}>
+            Sign up to get notified about our progress.
+          </p>
+          <p className={styles.description}>No b*llsh*t ads and nonsense mails.</p>
+          <p>
+            <label>
+              Email <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Sign me up</button>
+          </p>
+        </form>
       </main>
     </div>
   );
